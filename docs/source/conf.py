d1b2e9b9622e5666564728fe9c61aefed2c1a1a1
Xander Harris <xander@ecla.solutions>
2017-12-02 22:00:15 +0000

    Update docs. See #1.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 # import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import sys
+sys.path.insert(0, '/srv/accounting')
 
 
 # -- General configuration ------------------------------------------------
@@ -72,7 +72,7 @@ release = 'v0.0.1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'en' 
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
