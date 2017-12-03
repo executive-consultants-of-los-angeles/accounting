1f828fcfd7e49129e8cf735b50ac8345d6986eb5
Xander Harris <xander@ecla.solutions>
2017-12-03 07:25:50 +0000

    Update with env var. See #1.

--- transaction/transaction.py
+++ transaction/transaction.py
@@ -1,5 +1,7 @@
 """Module for transaction objects."""
+import os
 import yaml
+
 
 class Transaction(object):
     """A class for modeling accounting transactions.
@@ -7,16 +9,24 @@ class Transaction(object):
     .. attribute:: yml_file
 
        :description: file containing transactions
+
+    .. attribute:: yml_file_path
+
+       :description: path to yaml file
     """
     yml_file = ''
+    yml_file_path = ''
 
     def __init__(self):
         """Instantiate a Transaction object."""
+        self.yml_file_path = (
+            "{yml_file_path}/transaction.yml"
+        ).format(yml_file_path=os.environ.get('YML_PATH'))
         return
 
     def load_from_yaml(self):
         """Load values for transactions from yaml file."""
-        self.yml_file = open('/srv/accounting/transaction/yml/transaction.yml', 'r')
+        self.yml_file = open(self.yml_file_path, 'r')
         transactions_yml = self.yml_file.read()
         self.yml_file.close()
         transactions = yaml.load(transactions_yml)
