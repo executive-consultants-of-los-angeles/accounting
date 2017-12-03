c4133c5248b0be76d7266b05c683bdb8c7b6f126
Xander Harris <xander@ecla.solutions>
2017-12-03 08:15:59 +0000

    Fix pep8. See #1.

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
+"""Setup tools package information."""
 import os
 from setuptools import setup
 
 
 def read(fname):
-    """Utility function to read the README file.  Used for the long_description.  
-    It's nice, because now 
+    """Utility function to read the README file.  Used for the long_description.
+    It's nice, because now
 
-    0. we have a top level README file and 
+    0. we have a top level README file and
     0.  it's easier to type in the README file than to put a raw
 
      string in below ...
@@ -14,13 +15,13 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "accounting",
-    version = "0.0.1",
-    author = "ECLA",
-    author_email = "andrewjcarter@gmail.com",
-    description = ("Accounting software for accountants."),
-    license = "The Unlicense",
-    keywords = "accounting",
+    name="accounting",
+    version="0.0.1",
+    author="ECLA",
+    author_email="cfo@ecla.solutiosn",
+    description=("Accounting software for accountants."),
+    license="The Unlicense",
+    keywords="accounting",
     packages=['transaction', 'transaction.tests'],
     long_description=read('readme.md'),
     setup_requires=['pytest-runner'],
