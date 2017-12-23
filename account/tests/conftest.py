cd2f0bfda1aa3884bf58279ad4a5ca18592996f5
Xander Harris <xander@ecla.solutions>
2017-12-23 00:01:57 -0800

    Swap kind strings for ints. See #2.

---
+++ account/tests/conftest.py
@@ -0,0 +1,24 @@
+"""Make an Account fixture available."""
+import os
+import yaml
+import pytest
+
+from account.account import Account
+
+
+@pytest.fixture(scope='session')
+def account():
+    return Account()
+
+
+@pytest.fixture(scope='session')
+def accounts():
+    """Define accounts fixture."""
+    path = os.environ.get('YML_PATH')
+
+    yml_file = open('{}/account.yml'.format(path), 'r')
+    accounts_yml = yml_file.read()
+    yml_file.close()
+
+    accounts = yaml.load(accounts_yml)
+    return accounts
--- account/tests/test_account.py
+++ account/tests/test_account.py
@@ -1,16 +1,34 @@
 """Tests for the account module."""
+from account.account import Account
 
 
-def test_account_name():
-    """Make sure an account name contains only characters."""
-    assert False
+class TestAccount(object):
 
+    def test_account_object(self, account):
+        """Test account fixture object."""
+        assert isinstance(account, Account)
 
-def test_account_type():
-    """Account types should be [asset, liability, equity, revenue, expense."""
-    assert False
+    def test_account_name(self, accounts):
+        """Make sure an account name contains only characters."""
+        for item in accounts:
+            account = Account()
+            account.name = item.get('name')
+            account_name = account.get_name()
 
+            assert isinstance(account.name, str)
+            assert isinstance(account_name, str)
 
-def test_account_number():
-    """The account number should be an integer."""
-    assert False
+    def test_account_type(self, accounts):
+        """Account types should be
+        [asset, liability, equity, revenue, expense."""
+        kinds = ['asset', 'liability', 'equity', 'revenue', 'expense']
+        for item in accounts:
+            account = Account()
+            account.kind = account.kind
+
+        assert isinstance(account.kind, int)
+        assert kinds[account.kind] in kinds
+
+    def test_account_number(self):
+        """The account number should be an integer."""
+        assert False
