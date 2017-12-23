cdf4a049bb9c3bc9a0b769c1b3524a4d03d67f29
Xander Harris <xander@ecla.solutions>
2017-12-23 00:13:23 -0800

    Update transaction object. See #2.

--- transaction/transaction.py
+++ transaction/transaction.py
@@ -1,34 +1,41 @@
 """Module for transaction objects."""
-import os
-import yaml
+import datetime
 
 
 class Transaction(object):
-    """A class for modeling accounting transactions.
-
-    .. attribute:: yml_file
-
-       :description: file containing transactions
-
-    .. attribute:: yml_file_path
-
-       :description: path to yaml file
-    """
-    yml_file = ''
-    yml_file_path = ''
+    """A class for modeling accounting transactions."""
+    amount = float()
+    left_account = str()
+    right_account = str()
+    date = datetime.date
+    description = str()
+    tid = int()
 
     def __init__(self):
         """Instantiate a Transaction object."""
-        self.yml_file_path = (
-            "{yml_file_path}/transaction.yml"
-        ).format(yml_file_path=os.environ.get('YML_PATH'))
-        return
+        self.amount = float()
+        self.left_account = str()
+        self.right_account = str()
+        self.date = datetime.date
+        self.description = str()
+        self.tid = int()
 
-    def load_from_yaml(self):
-        """Load values for transactions from yaml file."""
-        self.yml_file = open(self.yml_file_path, 'r')
-        transactions_yml = self.yml_file.read()
-        self.yml_file.close()
-        transactions = yaml.load(transactions_yml)
-        print(transactions)
-        return transactions
+    def transaction_amount(self):
+        """Return amount for transaction."""
+        return self.amount
+
+    def transaction_left_account(self):
+        """Return the left (credit) account."""
+        return self.left_account
+
+    def transaction_right_account(self):
+        """Return the right (debit) account."""
+        return self.right_account
+
+    def transaction_description(self):
+        """Return the description."""
+        return self.description
+
+    def transaction_tid(self):
+        """Return the tid."""
+        return self.tid
