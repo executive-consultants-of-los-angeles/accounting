0a61d7d5d468df1a577d093b07b1a98785df663e
Xander Harris <xander@ecla.solutions>
2017-12-22 23:27:05 -0800

    Start on tests. See #2.

---
+++ account/tests/test_account.py
@@ -0,0 +1,16 @@
+"""Tests for the account module."""
+
+
+def test_account_name():
+    """Make sure an account name contains only characters."""
+    assert False
+
+
+def test_account_type():
+    """Account types should be [asset, liability, equity, revenue, expense."""
+    assert False
+
+
+def test_account_number():
+    """The account number should be an integer."""
+    assert False
