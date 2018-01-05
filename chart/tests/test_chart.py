fa8681f08df8e8fa2c2b823daa792ace79656ee1
Xander Harris <duchess@gahan-corporation.com>
2018-01-05 09:01:00 +0000

    Pass lint. See #3.

--- chart/tests/test_chart.py
+++ chart/tests/test_chart.py
@@ -5,23 +5,26 @@ from chart.chart import Chart
 class TestChart(object):
     """Test class for chart module."""
 
+    chart = Chart
+
     def test_chart_object(self, chart):
         """Test chart fixture object."""
-        assert isinstance(chart, Chart)
+        assert isinstance(chart, self.chart)
 
     def test_chart_name(self, chart):
         """Test the chart's name."""
-        chart = Chart()
+        chart = self.chart()
         chart.name = 'Test Chart'
         chart_name = chart.name
-        
+
         assert isinstance(chart.name, str)
         assert isinstance(chart_name, str)
 
-    def test_chart_related_accounts(self, charts):
+    def test_chart_related_accounts(self, chart):
         """Test chart assignment of accounts to a side of the equation."""
-        chart = Chart()
+        chart = self.chart()
         chart.accounts = []
         chart_accounts = chart.accounts
 
-        assert False
+        assert isinstance(chart.accounts, list)
+        assert isinstance(chart_accounts, list)
