a7ef5bf74d382067176e5787c854ce9b67d52126
Xander Harris <duchess@gahan-corporation.com>
2018-02-01 10:20:45 -0800

    Remove html. See #22.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -19,9 +19,11 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
+import os
+import sys
 import django
+
+sys.path.append(os.path.join(os.path.dirname(__name__), '../..'))
 
 
 django.setup()
