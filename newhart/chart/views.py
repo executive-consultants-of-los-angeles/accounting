4523a039b31cfc15cb6a717fe51719376a0ba077
Xander Harris <duchess@gahan-corporation.com>
2018-02-02 04:11:15 -0800

    Add template. See #4.

--- newhart/chart/views.py
+++ newhart/chart/views.py
@@ -1,3 +1,9 @@
+"""Views module for chart app."""
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    """Render the index page."""
+
+    return render(request, 'chart/index.html')
