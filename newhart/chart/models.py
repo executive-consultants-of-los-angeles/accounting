92cbd57df74098b4b45e8611b79981314612452d
Xander Harris <duchess@gahan-corporation.com>
2018-02-01 23:34:29 -0800

    Start transition. See #24.

--- newhart/chart/models.py
+++ newhart/chart/models.py
@@ -1,3 +1,11 @@
+"""Module for the chart of accounts class. (Chart)"""
 from django.db import models
 
-# Create your models here.
+class Chart(models.Model):
+    """Class for Chart objects."""
+
+    name = models.CharField("Chart", max_length=255)
+
+    def assemble_chart(self):
+        """This should pull a workable chart from the data."""
+        return self.name
