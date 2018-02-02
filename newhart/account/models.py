510b9ea9512bc8aa700efad847ba06f906236262
Xander Harris <duchess@gahan-corporation.com>
2018-02-01 22:51:13 -0800

    Update models. See #23.

--- newhart/account/models.py
+++ newhart/account/models.py
@@ -1,3 +1,22 @@
+"""Module for the account model."""
 from django.db import models
 
 # Create your models here.
+class Account(models.Model):
+    """Class that defines account objects."""
+
+    name = models.CharField("Name", maxlength=255)
+    kind = 0
+    number = models.IntegerField("Number")
+
+    def account_name(self):
+        """Return account name."""
+        return self.name
+
+    def account_kind(self):
+        """Return the integer value of the account kind."""
+        return self.kind
+
+    def account_number(self):
+        """Return the account number."""
+        return self.number
