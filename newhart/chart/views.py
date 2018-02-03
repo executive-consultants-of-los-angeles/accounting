4b258dd435c80c81231e9de3befe73f6c2a7e577
Xander Harris <duchess@gahan-corporation.com>
2018-02-02 17:35:59 -0800

    Update view with context. See #4.

--- newhart/chart/views.py
+++ newhart/chart/views.py
@@ -1,9 +1,20 @@
 """Views module for chart app."""
+# pylint: disable=no-member
 from django.shortcuts import render
+
+from chart.models import Chart
+from account.models import Account
 
 # Create your views here.
 
 def index(request):
     """Render the index page."""
+    context = {
+        'charts': {
+            Chart.objects.get(pk=1).name: {
+                'accounts': [account for account in Account.objects.all()]
+            }
+        }
+    }
 
-    return render(request, 'chart/index.html')
+    return render(request, 'chart/index.html', context)
